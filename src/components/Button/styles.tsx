35999e753050d6847bb73fada25fa553fb1a673f
davidrockkj <davidrockkj@gmail.com>
2024-03-02 16:55:33 -0300

    Estilizando 'Button'

--- src/components/Button/styles.tsx
+++ src/components/Button/styles.tsx
@@ -1,5 +1,22 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {}
+  container: {
+    flex: 1,
+  },
+  button: {
+    borderColor: '#213E98',
+    borderWidth: 3,
+    padding: 10,
+    paddingHorizontal: 30,
+    borderRadius: 10,
+  },
+  textButton: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#213E98',
+    textAlign: 'center',
+    shadowColor: '#000',
+    textShadowColor: '#000',
+  }
 });
