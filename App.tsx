b9c8c80d42eafe4b34aa2537bc0c4e311c67119c
davidrockkj <davidrockkj@gmail.com>
2024-03-02 14:11:28 -0300

    Implementando 'Login' e 'InputComLabel'

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 
-
+import Login from './src/screens/login';
 
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="dark" />
+      <Login />
     </View>
   );
 }
--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { View } from 'react-native';
 
 import { styles } from './styles';
+import { InputComLabel } from '../../components/InputComLabel';
 
-export function login() {
+export default function login() {
   return (
     <View style={styles.container}>
-
+      <InputComLabel 
+        label='Matrícula' 
+        placeholder='Digite sua matrícula' 
+        secureTextEntry='false'
+      />
     </View>
   );
 }
