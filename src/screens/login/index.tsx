6dcddc262f85113b711fbc86a2ed9a7023b31602
davidrockkj <davidrockkj@gmail.com>
2024-03-02 18:19:44 -0300

    COlocando o logo da facape

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -5,26 +5,33 @@ import { styles } from './styles';
 import { InputComLabel } from '../../components/InputComLabel';
 import { Button } from '../../components/Button';
 import JardimFacape from '../../assets/jardimFacape.png';
+import LogoFacape from '../../assets/logoFacape.png'
 
 export default function login() {
   return (
     <View style={styles.container}>
       <Image source={JardimFacape} style={styles.background}/>
 
-      <InputComLabel 
-        label='Matrícula' 
-        placeholder='00000' 
-        secureTextEntry='false'
-      />
+      <View style={styles.content}>
+        <View style={styles.logoContainer}>
+          <Image source={LogoFacape} style={styles.logo}/>
+        </View>
 
-      <InputComLabel 
-        label='Senha' 
-        placeholder='Digite sua senha' 
-        secureTextEntry='true'
-      />
+        <InputComLabel 
+          label='Matrícula' 
+          placeholder='00000' 
+          secureTextEntry='false'
+        />
 
-      <View style={styles.buttonContainer}>
-        <Button buttonName='Entrar' />
+        <InputComLabel 
+          label='Senha' 
+          placeholder='Digite sua senha' 
+          secureTextEntry='true'
+        />
+
+        <View style={styles.buttonContainer}>
+          <Button buttonName='Entrar' />
+        </View>
       </View>
 
     </View>
--- src/screens/login/styles.tsx
+++ src/screens/login/styles.tsx
@@ -4,19 +4,38 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     width: '50%',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-end',
+  },
+  content: {
+    flex: 1,
+    width: '100%',
+    marginTop: 50,
     alignItems: 'flex-end',
   },
   buttonContainer: {
     marginTop: 20,
     width: '60%',
     height: 50,
+    justifyContent: 'flex-end',
   },
   background: {
     flex: 1,
     resizeMode: 'cover',
-    justifyContent: 'center',
     position: 'absolute',
     left: -100,
-  }
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logoContainer: {
+    width: '100%',
+    height: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 50,
+  },
 });
