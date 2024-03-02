fcd56c8277a9db6e4f15f2ae6d90ab6cead58d0e
davidrockkj <davidrockkj@gmail.com>
2024-03-02 14:07:26 -0300

    InputComLabel: criando estrutura

--- src/components/InputComLabel/index.tsx
+++ src/components/InputComLabel/index.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, Text } from 'react-native';
 
 import { styles } from './styles';
 
-export function InputComLabel() {
+type InputComLabelProps = {
+  label: string;
+  placeholder: string;
+  secureTextEntry: 'true' | 'false';
+}
+
+export function InputComLabel( { label, placeholder, secureTextEntry}: InputComLabelProps) {
+
+  const [blur, setBlur] = useState(false);
+
   return (
     <View style={styles.container}>
-
+      <Text style={styles.labelText}>{label}</Text>
+      <TextInput
+        style={[styles.input, blur ? {} : styles.inputFocus ]}
+        placeholder={placeholder || 'Digite aqui'}
+        secureTextEntry={secureTextEntry === 'true' ? true : false}
+        onFocus={ () => setBlur(true)}
+        onBlur={ () => setBlur(false)}
+      />
     </View>
   );
 }
